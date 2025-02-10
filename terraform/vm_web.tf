aa9b6a4c741e98c1a10c848806c5084be2cb1dee
Sebastian Högberg <all.my.stuff.sl93@gmail.com>
2025-02-10 22:19:34 +0100

    Upgrade webserver computed

--- terraform/vm_web.tf
+++ terraform/vm_web.tf
@@ -2,7 +2,7 @@
 
 resource "openstack_blockstorage_volume_v3" "webserver-boot-vol" {
   name              = "web-boot-vol"
-  size              = 8
+  size              = 40
   image_id          = var.image["ubu22"]
   description       = "Boot volume for web"
   availability_zone = var.zone[1]
@@ -11,7 +11,7 @@ resource "openstack_blockstorage_volume_v3" "webserver-boot-vol" {
 resource "openstack_compute_instance_v2" "webserver" {
   name              = "webserver"
   image_id          = var.image["ubu22"]
-  flavor_id         = var.flavor["m1.small"]
+  flavor_id         = var.flavor["m1.xlarge"]
   key_pair          = openstack_compute_keypair_v2.keypair.name
   security_groups   = ["sg-web-server"]
   availability_zone = var.zone[1]
